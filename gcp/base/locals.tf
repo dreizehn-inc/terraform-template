a52023abf7babc30ba8ba31192605b76d6eddff8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-05-05 22:26:15 +0900

    feat: add gcs

--- gcp/base/locals.tf
+++ gcp/base/locals.tf
@@ -1,8 +1,9 @@
 locals {
-  project             = ""
-  location            = ""
-  env                 = ""
-  tf_state_bucket     = ""
-  repository_id       = ""
-  github_repositories = []
+  project               = ""
+  location              = ""
+  env                   = ""
+  tf_state_bucket       = ""
+  repository_id         = ""
+  github_repositories   = []
+  gcs_asset_bucket_name = ""
 }
