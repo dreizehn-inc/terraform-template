c9d01682e5e7bbce1a38c93538b5101e98f1ab23
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-05-05 22:54:25 +0900

    feat: add dependes on

--- gcp/base/artifact_registry.tf
+++ gcp/base/artifact_registry.tf
@@ -2,4 +2,8 @@ resource "google_artifact_registry_repository" "registry" {
   location      = local.location
   repository_id = local.repository_id
   format        = "DOCKER"
+
+  depends_on = [
+    google_project_service.service,
+  ]
 }
