fcc9ae8e6eefa9385bf48953ceb7c40db82dfe69
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-05-05 22:39:50 +0900

    feat: add cloud sql

---
+++ gcp/base/cloud_sql.tf
@@ -0,0 +1,38 @@
+resource "google_sql_database_instance" "master_instance" {
+  name             = local.db_master_instance_name
+  database_version = "MYSQL_8_0"
+  region           = local.location
+
+  settings {
+    tier              = local.db_tier
+    disk_type         = local.db_disk_type
+    availability_type = local.db_availability_type
+
+    database_flags {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+
+    backup_configuration {
+      location           = "asia"
+      enabled            = true
+      binary_log_enabled = true
+    }
+
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = local.db_database_name
+  instance = google_sql_database_instance.master_instance.name
+}
+
+resource "random_password" "db_password" {
+  length = 16
+}
+
+resource "google_sql_user" "app_user" {
+  name     = local.db_database_user_name
+  instance = google_sql_database_instance.master_instance.name
+  password = random_password.db_password.result
+}
