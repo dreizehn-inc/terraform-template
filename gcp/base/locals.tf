3d78f02043b5d5ba334ccea0598a90592b36e38d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-05-17 05:07:16 +0900

    refactor: some gcp components

--- gcp/base/locals.tf
+++ gcp/base/locals.tf
@@ -2,20 +2,11 @@ locals {
   project               = ""
   location              = ""
   env                   = ""
-  tf_state_bucket       = ""
-  repository_id         = ""
   github_repositories   = []
-  vpc_network_name      = ""
   gcs_asset_bucket_name = ""
-
-
-  db_master_instance_name    = ""
-  db_database_name           = ""
-  db_database_user_name      = ""
-  db_tier                    = ""
-  db_disk_type               = ""
-  db_availability_type       = ""
-  db_password_secret_version = "1"
+  db_tier               = ""
+  db_disk_type          = ""
+  db_availability_type  = ""
 }
 
 data "sops_file" "secrets" {
