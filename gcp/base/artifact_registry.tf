5138818c553133e16cb90e3e7f73afe20d7b6cef
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-05-17 05:07:09 +0900

    refactor: some gcp components

--- gcp/base/artifact_registry.tf
+++ gcp/base/artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "registry" {
   location      = local.location
-  repository_id = local.repository_id
+  repository_id = "registry"
   format        = "DOCKER"
 
   depends_on = [
